ded625cb273195706fe215b31a43de417cbf6868
George Daian <georgedaianx@gmail.com>
2023-11-21 16:33:55 +0200

    Read data from the connection and respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf: Vec<u8> = Vec::new();
+                let _request_bytes = stream.read_to_end(&mut buf);
+
+                let response: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
+                stream.write(response);
             }
             Err(e) => {
                 println!("error: {}", e);

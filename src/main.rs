1df379f38aad76c671fe80f8bd430aebf4396640
George Daian <georgedaianx@gmail.com>
2023-11-21 16:40:25 +0200

    Extract handle_connection function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf: Vec<u8> = Vec::new();
+    let _request_bytes = stream.read_to_end(&mut buf);
+
+    match stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n") {
+        Ok(_) => (),
+        Err(e) => println!("Error in handle connection: {}", e),
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let mut buf: Vec<u8> = Vec::new();
-                let _request_bytes = stream.read_to_end(&mut buf);
-
-                let response: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
-                stream.write(response);
+                handle_connection(stream)
             }
             Err(e) => {
                 println!("error: {}", e);

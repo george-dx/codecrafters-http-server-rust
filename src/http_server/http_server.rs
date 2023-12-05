696debbebf34269bf6e26d88176e6926b02df7a0
George Daian <georgedaianx@gmail.com>
2023-12-05 14:52:23 +0200

    Add concurrent connections, extract handle_request and handle_response as a temporary solution

--- src/http_server/http_server.rs
+++ src/http_server/http_server.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{BufRead, BufReader, Read, Write},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream}, thread
 };
 
 use itertools::Itertools;
@@ -26,7 +26,7 @@ impl HttpServer {
             match stream {
                 Ok(stream) => {
                     println!("accepted new connection");
-                    self.handle_request(stream)
+                    thread::spawn(move || handle_request(stream));
                 }
                 Err(e) => {
                     println!("Error on tcp stream: {}", e);
@@ -43,60 +43,61 @@ impl HttpServer {
         println!("Requested lines: {:?}", request_bytes);
     }
 
-    fn handle_request(&self, mut stream: TcpStream) {
-        let buffer: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
-        let request_bytes: Vec<String> = buffer
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect_vec();
-        // println!("Requested lines: {:?}", request_bytes);
-        self.handle_response(request_bytes, stream);
-    }
+}
 
-    fn handle_response(&self, request_lines: Vec<String>, mut stream: TcpStream) {
-        let start_line = request_lines.get(0).expect("Missing first line");
-        let start_line_parts: Vec<&str> = start_line.split(' ').collect();
-        let path = start_line_parts
-            .get(1)
-            .unwrap_or(&"Missing start part get path");
+fn handle_request(mut stream: TcpStream) {
+    let buffer: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
+    let request_bytes: Vec<String> = buffer
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect_vec();
+    // println!("Requested lines: {:?}", request_bytes);
+    handle_response(request_bytes, stream);
+}
 
-        let parts: Vec<&str> = path.split("/echo/").collect();
-        println!("{:?}", parts);
-        match path {
-            &"/" => match stream.write_all(OK_RESPONSE) {
-                Ok(_) => (),
-                Err(e) => println!("Error on ok response: {}", e),
-            },
-            path if path.starts_with("/echo/") => {
-                let echo_message_parts: Vec<&str> = path.split("/echo/").collect();
-                let echo_message = echo_message_parts.get(1).unwrap();
-                let response = format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    echo_message.len(),
-                    echo_message
-                );
-                let _ = stream.write_all(response.as_bytes()).unwrap();
-            }
-            path if path.starts_with("/user-agent") => {
-                let mut user_agent_line = String::new();
-                for element in request_lines {
-                    if element.contains(&"User-Agent") {
-                        user_agent_line = element.to_string();
-                    }
-                }
-                let user_agent: Vec<&str> = user_agent_line.split(": ").collect();
-                let response = format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
-                    user_agent.get(1).expect("Missing user agent").len(),
-                    user_agent.get(1).expect("Missing user agent")
-                );
-                let _ = stream.write_all(response.as_bytes()).unwrap();
-            }
-            _ => match stream.write_all(NOT_FOUND_RESPONSE) {
-                Ok(_) => (),
-                Err(e) => println!("Error on not found response: {}", e),
-            },
+fn handle_response(request_lines: Vec<String>, mut stream: TcpStream) {
+    let start_line: &String = request_lines.get(0).expect("Missing first line");
+    let start_line_parts: Vec<&str> = start_line.split(' ').collect();
+    let path = start_line_parts
+        .get(1)
+        .unwrap_or(&"Missing start part get path");
+
+    let parts: Vec<&str> = path.split("/echo/").collect();
+    println!("{:?}", parts);
+    match path {
+        &"/" => match stream.write_all(OK_RESPONSE) {
+            Ok(_) => (),
+            Err(e) => println!("Error on ok response: {}", e),
+        },
+        path if path.starts_with("/echo/") => {
+            let echo_message_parts: Vec<&str> = path.split("/echo/").collect();
+            let echo_message = echo_message_parts.get(1).unwrap();
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                echo_message.len(),
+                echo_message
+            );
+            let _ = stream.write_all(response.as_bytes()).unwrap();
         }
+        path if path.starts_with("/user-agent") => {
+            let mut user_agent_line = String::new();
+            for element in request_lines {
+                if element.contains(&"User-Agent") {
+                    user_agent_line = element.to_string();
+                }
+            }
+            let user_agent: Vec<&str> = user_agent_line.split(": ").collect();
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
+                user_agent.get(1).expect("Missing user agent").len(),
+                user_agent.get(1).expect("Missing user agent")
+            );
+            let _ = stream.write_all(response.as_bytes()).unwrap();
+        }
+        _ => match stream.write_all(NOT_FOUND_RESPONSE) {
+            Ok(_) => (),
+            Err(e) => println!("Error on not found response: {}", e),
+        },
     }
 }

eabf0e07a4fb6bbf26bf9ad1e5db58548be04b29
George Daian <georgedaianx@gmail.com>
2024-01-09 11:54:54 +0200

    Extract 201 response in global variable, remove unused code

--- src/http_server/http_server.rs
+++ src/http_server/http_server.rs
@@ -1,6 +1,7 @@
 use itertools::Itertools;
 use std::{
-    env, fs::{self, File},
+    env,
+    fs::{self, File},
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
     path::{Path, PathBuf},
@@ -9,6 +10,7 @@ use std::{
 
 const OK_RESPONSE: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
 const NOT_FOUND_RESPONSE: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
+const OK_CREATED_RESPONSE: &[u8] = b"HTTP/1.1 201 Created\r\n\r\n";
 
 pub struct HttpServer {
     ip_addr: String,
@@ -38,7 +40,7 @@ impl HttpServer {
     }
 }
 
-fn handle_request(mut stream: TcpStream) {
+fn handle_request(stream: TcpStream) {
     let mut stream_clone = stream.try_clone().expect("Failed to clone stream");
     let mut buffer: BufReader<&mut TcpStream> = BufReader::new(&mut stream_clone);
     let buffer_u8 = buffer.fill_buf().unwrap();
@@ -46,17 +48,17 @@ fn handle_request(mut stream: TcpStream) {
 }
 
 fn handle_response(mut stream: TcpStream, buffer_u8: &[u8]) {
-    let request_lines =  buffer_u8.lines()
-    .map(|result| result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect_vec();
+    let request_lines = buffer_u8
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect_vec();
     let start_line: &String = request_lines.get(0).expect("Missing first line");
     let start_line_parts: Vec<&str> = start_line.split(' ').collect();
     let path = start_line_parts
         .get(1)
         .unwrap_or(&"Missing start part get path");
 
-    let parts: Vec<&str> = path.split("/echo/").collect();
     match path {
         &"/" => match stream.write_all(OK_RESPONSE) {
             Ok(_) => (),
@@ -91,7 +93,6 @@ fn handle_response(mut stream: TcpStream, buffer_u8: &[u8]) {
             let cmd_args: Vec<String> = env::args().collect();
             let directory_path = &cmd_args[2];
             let dir = fs::read_dir(directory_path).expect("Could not read the directory");
-            // let paths = fs::read_dir(directory_path).expect("Could not read the directory");
             let message_parts: Vec<&str> = path.split("/files/").collect();
             let filename = message_parts.get(1).expect(&"Missing file name");
             if start_line_parts[0] == "GET" {
@@ -116,33 +117,29 @@ fn handle_response(mut stream: TcpStream, buffer_u8: &[u8]) {
                             let _ = stream.write_all(response.as_bytes()).unwrap();
                         }
                         Err(..) => {
-                            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                            let _ = stream.write_all(response.as_bytes()).unwrap();
+                            let _ = stream.write_all(NOT_FOUND_RESPONSE).unwrap();
                         }
                     }
                 } else {
-                    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                    let _ = stream.write_all(response.as_bytes()).unwrap();
+                    let _ = stream.write_all(NOT_FOUND_RESPONSE).unwrap();
                 }
-            } else if start_line_parts[0] == "POST" 
-            {
+            } else if start_line_parts[0] == "POST" {
                 let mut fpath = PathBuf::new();
                 fpath.push(directory_path);
                 fpath.push(filename);
                 let mut file = File::create(fpath).expect("Error when creating file");
                 let result = String::from_utf8(buffer_u8.to_vec()).unwrap();
                 let body: String = result
-            .splitn(2, "\r\n\r\n")
-            .nth(1)
-            .map(|s| s.to_string())
-            .unwrap_or_else(|| "No Body".to_string());
+                    .splitn(2, "\r\n\r\n")
+                    .nth(1)
+                    .map(|s| s.to_string())
+                    .unwrap_or_else(|| "No Body".to_string());
                 if body == "No Body" {
-                    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                    stream.write_all(response.as_bytes()).unwrap();
+                    stream.write_all(NOT_FOUND_RESPONSE).unwrap();
                 } else {
-                    file.write(body.as_bytes()).expect("Could not write to file");
-                    let response = "HTTP/1.1 201 Created\r\n\r\n";
-                    let _ = stream.write(response.as_bytes()).unwrap();
+                    file.write(body.as_bytes())
+                        .expect("Could not write to file");
+                    let _ = stream.write(OK_CREATED_RESPONSE).unwrap();
                 }
             }
         }
